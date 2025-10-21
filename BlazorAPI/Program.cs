460f153bc049c4bcd00899a4031fd95a3bb78800
hoangbao.nguyen <hoangbao.nguyen@asiaesolutions.com>
2025-10-21 09:38:23 +0700

    ,

--- BlazorAPI/Program.cs
+++ BlazorAPI/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
@@ -26,63 +28,69 @@ namespace BlazorAPI
             }
             ).AddXmlDataContractSerializerFormatters();
 
-            builder.Services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "Bao API 127 v1",
-                    Version = "v1",
-                    Description = "An API to demo upload document",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "nglehoangbao",
-                        Email = "nglehoangbao@gmail.com",
-                        Url = new Uri("https://twitter.com/jwalkner"),
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = "License ne",
-                        Url = new Uri("https://example.com/license"),
-                    }
-                });
+            //builder.Services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo
+            //    {
+            //        Title = "Bao API 127 v1",
+            //        Version = "v1",
+            //        Description = "An API to demo upload document",
+            //        Contact = new OpenApiContact
+            //        {
+            //            Name = "nglehoangbao",
+            //            Email = "nglehoangbao@gmail.com",
+            //            Url = new Uri("https://twitter.com/jwalkner"),
+            //        },
+            //        License = new OpenApiLicense
+            //        {
+            //            Name = "License ne",
+            //            Url = new Uri("https://example.com/license"),
+            //        }
+            //    });
+            //    // Set the comments path for the Swagger JSON and UI.
+            //    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            //    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            //    c.IncludeXmlComments(xmlPath);
 
-                // Set the comments path for the Swagger JSON and UI.
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
-            });
-            builder.Services.AddSwaggerGen(options =>
+            //    #region Bearer Authen 
+            //    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            //    {
+            //        Description =
+            //            "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
+            //            "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+            //            "Example: \"12345abcdef\"",
+            //        Name = "Authorization",
+            //        In = ParameterLocation.Header,
+            //        Scheme = "Bearer"
+            //    });
+            //    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            //    {
+            //        {
+            //            new OpenApiSecurityScheme
+            //            {
+            //                Reference = new OpenApiReference
+            //                {
+            //                    Type = ReferenceType.SecurityScheme,
+            //                    Id = "Bearer"
+            //                },
+            //                Scheme = "oauth2",
+            //                Name = "Bearer",
+            //                In = ParameterLocation.Header
+            //            },
+            //            new List<string>()
+            //        }
+            //    });
+            //    #endregion
+            //});
+
+            #region Custome Auth 
+            builder.Services.AddAuthentication(x =>
             {
-               #region Bearer Authen 
-                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description =
-                        "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
-                        "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
-                        "Example: \"12345abcdef\"",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Scheme = "Bearer"
-                });
-                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                            Scheme = "oauth2",
-                            Name = "Bearer",
-                            In = ParameterLocation.Header
-                        },
-                        new List<string>()
-                    }
-                });
-                #endregion
-            });
+                x.DefaultAuthenticateScheme = "GoogleAccessToken";
+            })
+            .AddScheme<AuthenticationSchemeOptions, GoogleAccessTokenAuthenticationHandler>("GoogleAccessToken", null);
+            #endregion
+
             builder.Services.AddHttpContextAccessor();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
